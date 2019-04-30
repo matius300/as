4d7ffe2e7fff9ea009d2b74cb6c285a9237f81ea
surham <33297541+surham@users.noreply.github.com>
2019-04-30 07:47:07 +0700

    Add files via upload

---
+++ 441.php
@@ -0,0 +1,102 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link rel="stylesheet" type="text/css" href="/website.css">
+
+ <title>Faucet Overloaded</title>
+ <?php include $_SERVER['DOCUMENT_ROOT'] . '/custom/head.php'; ?>
+
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-radius: 25px;
+}
+</style>
+
+</head> 
+            
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/bannernavbar.php';?>
+
+
+
+<div class="row">
+  <div class="column side">
+<center>
+<p><b>Recommended by me!</b></p>
+
+</center>
+  </div>
+  
+  
+  
+  
+  <div class="column middle">
+<center><table width="99%" bgcolor="#F2F3F4"></center>
+  <tr>
+    <td><center><h2>Welcome</h2></center>
+	 <h1>Faucet Overloaded</h1>
+ <p>Please wait a few minutes and try again.</p>
+ <h2>Why?</h2>
+ <p>According to FaucetHUB:</p>
+ <blockquote>
+  <p>If you send too many API requests in a given time frame your API key will be blocked from making further requests for a short time.</p>
+  <p>The following limits are in place for all users:</p>
+  <ul>
+   <li>120 requests per 1 mins</li>
+   <li>1000 requests per 10 mins</li>
+   <li>4000 requests per 1 hours</li>
+  </ul>
+ </blockquote>
+</td>
+  </tr>
+</table>  
+
+
+<center><table width="99%" bgcolor="#F2F3F4"></center>
+  <tr>
+    <td><center><p><b><h2>Latest News!</h2></b></p></center>
+<center>
+<p>NEW SPACE CRYPTO GAME!!  <a href="http://starcoins.ws/r/432" target="_blank">Star Coins!!</a></p>
+<p><a href="https://cointiply.com/r/kO86" target="_blank">Cointiply!  Earn money and great referral program!</a></p>
+<p style="margin:0;"><h2>New Faucets and Auto Faucets on a </p><p><a href="http://findfreecrypto.com/" target="_blank">New Website!</a></h2></p><br>
+<a href="https://coinraffles.com/?r=passiveearn" target="_blank">COINRAFFLES!!</a><br>
+<p><a href="https://www.epay.com/?ref=755573" target="_blank">Epay</a></p>
+<div class="adbit-display-ad" data-adspace-id="CFAC3D954B"></div>
+
+
+
+<br><br><br>
+<a href="http://potcoinlotto.com/?ref=japakar" target="_blank"><img src="http://potcoinlotto.com/i/468x60-1.png"></a>
+</center>
+</td>
+  </tr>
+</table>
+
+  </div>
+  
+  
+  
+  <div class="column side">
+  
+<center>
+<center>
+<p><b>Recommended by me!</b></p>
+
+</center>
+  </div>
+</div>
+
+
+
+<center>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/ads/bottomfooterref.php';?>
+<br><br>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/ads/copyright.php';?>
+</center>
+
+</body>
+</html>
---
+++ bannernavbar.php
@@ -0,0 +1,161 @@
+<center>
+<style>
+
+.dropbtn {
+
+    background-color: #85c1e9;
+
+    color: white;
+
+    padding: 5px;
+
+    font-size: 16px;
+
+    border-radius: 15px 15px 15px 15px;
+
+    cursor: pointer;
+
+
+
+}
+
+
+
+.dropdown {
+
+    position: relative;
+
+    display: inline-block;
+
+
+}
+
+
+
+.dropdown-content {
+
+    display: none;
+
+    position: absolute;
+
+    background-color: #85c1e9;
+
+    min-width: 160px;
+
+    box-shadow: 0px 8px 8px 0px rgba(0,0,0,0.2);
+
+    z-index: 1;
+
+	border-radius: 15px 15px 15px 15px;
+
+}
+
+
+
+.dropdown-content a {
+
+    color: white;
+
+    padding: 5px 5px;
+
+    text-decoration: none;
+
+    display: block;
+	
+	border-radius: 15px 15px 15px 15px;
+
+}
+
+
+
+.dropdown-content a:hover {background-color:  #2471a3}
+
+
+
+.dropdown:hover .dropdown-content {
+
+    display: block;
+
+}
+
+
+
+.dropdown:hover .dropbtn {
+
+    background-color: #a9cce3;
+	
+}
+
+</style>
+</div>
+
+</head>
+
+<body>
+<div class="footer9">
+
+
+<div class="dropdown">
+
+     <a href="http://japakar.com/"><button class="dropbtn">HOME</button></a>
+
+  <div class="dropdown-content">
+
+
+
+  </div>
+
+</div>
+
+    
+
+<div class="dropdown">
+
+  <button class="dropbtn">My Auto Faucets</button>
+
+  <div class="dropdown-content">
+<p><b>Use ALL at the same time!</p></b>
+
+  </div>
+
+</div>
+
+
+
+
+<div class="dropdown">
+
+  <button class="dropbtn">Faucet Lists</button>
+
+  <div class="dropdown-content">
+<p><b>Other Faucets</p></b>
+
+  </div>
+
+</div>
+
+
+
+
+
+<div class="dropdown">
+
+  <button class="dropbtn">Guides</button>
+
+  <div class="dropdown-content">
+
+
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+</center>
+</a></p>
